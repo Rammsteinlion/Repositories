a67db2136707b901eef92aa3b622e8f38de8bf5e
elkin <rastaw28@gmail.com>
2021-03-23 15:21:52 -0500

    Agregado de dependencias

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" type="image/ico" href="public/img/icono/phonebook_notebook_14401.ico" sizes="64x64">
+    <?php include_once "dependencias.php"; ?>
+    <title>Agenda Web</title>
+</head>
+<body>
+    
+</body>
+</html>
